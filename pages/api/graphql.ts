455e8faf56dcaf752fb922a45392a0aa961c36b6
TylerSustare <tylersustare@gmail.com>
2020-06-21 13:50:15 -0700

    Init Next app with TS and GQL

---
+++ pages/api/graphql.ts
@@ -0,0 +1,51 @@
+// https://github.com/apollographql/apollo-server/tree/master/packages/apollo-server-micro
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+interface GqlContext {
+  favorite: string;
+}
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    name: String
+    favorite: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users(_parent: undefined, _args: undefined, context: GqlContext) {
+      console.log('favorite', context.favorite);
+      return [{ name: 'Tyler', favorite: context.favorite }];
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }): GqlContext => {
+    const {
+      headers: { favorite },
+    } = req as NextApiRequest;
+    return { favorite: favorite as string };
+  },
+});
+
+const handler = apolloServer.createHandler({ path: '/api/graphql' });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// how to run "middleware" with this setup
+export default function (req: NextApiRequest, res: NextApiResponse) {
+  req.headers.favorite = 'flapjack';
+  return handler(req, res);
+}
